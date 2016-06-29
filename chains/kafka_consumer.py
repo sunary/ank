bb15fa8088d735a71cdd1bc337f678ddbf3c7c20
sunary <v2nhat@gmail.com>
2016-06-29 17:47:16 +0700

    add kafka

--- chains/kafka_consumer.py
+++ chains/kafka_consumer.py
@@ -1,1 +1,31 @@
 __author__ = 'sunary'
+
+
+from apps._app import App
+try:
+    from kafka import KafkaConsumer
+except:
+    raise 'kafka not founded'
+
+
+class KafkaAnkConsumer(App):
+    '''
+    Messages were received from consumer.subscribe
+    '''
+
+    def __init__(self, queue_config, topic):
+        super(KafkaAnkConsumer, self).__init__()
+
+        self.consumer = KafkaConsumer(queue_config)
+        self.topic = topic
+
+    def run(self, process=None):
+        super(KafkaAnkConsumer, self).run(process)
+
+        self.consumer.subscribe(self.topic)
+
+        for message in self.consumer:
+            self._process(message)
+
+    def process(self, messages=None):
+        return messages
--- chains/kafka_producer.py
+++ chains/kafka_producer.py
@@ -1,1 +1,25 @@
 __author__ = 'sunary'
+
+
+from apps._app import App
+try:
+    from kafka import KafkaProducer
+except:
+    raise 'kafka not founded'
+
+
+class KafkaAnkProducer(App):
+    '''
+    Post message(s) to queue
+    '''
+
+    def __init__(self, queue_config, topic):
+        super(KafkaAnkProducer, self).__init__()
+
+        self.producer = KafkaProducer(queue_config)
+        self.topic = topic
+
+    def process(self, messages=None):
+        self.producer.send(self.topic, messages)
+
+        return messages
