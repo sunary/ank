632e96e42989685c4d991f5478e30f036b378b3e
sunary <v2nhat@gmail.com>
2016-12-20 07:59:26 +0700

    create example

---
+++ examples/schedule_app/_processor.py
@@ -0,0 +1,9 @@
+__author__ = 'ank_generator'
+
+from chains.chain_processor import ChainProcessor
+from processor import ScheduleExample
+
+chain_processor = ChainProcessor()
+_schedule_example = ScheduleExample('0 0 * * 5', True)
+chain_processor.add_processor(_schedule_example)
+_schedule_example.run(chain_processor.process)
