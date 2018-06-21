e1674da6d683e137422972ca727b5b680669c547
Nhat Vo Van <sunary@Nhats-MacBook-Pro.local>
2018-06-21 07:23:34 +0700

    support redis pubsub as default app

---
+++ apps/redis_subscribe.py
@@ -0,0 +1,31 @@
+__author__ = 'sunary'
+
+
+from base_apps.pipe_app import PipeApp
+
+
+class RedisSubscribe(PipeApp):
+    """
+    Subscribe message from redis pubsub
+    """
+
+    def ini_app(self, redis=None, topic=''):
+        """
+        Args:
+            redis (redis.StrictRedis): redis client
+            topic (string): redis subscriber topic
+        """
+        self.redis = redis
+        self.topic = topic
+
+    def start(self):
+        self.logger.info('Start {}'.format(self.__class__.__name__))
+        pubsub = self.redis.pubsub(self.topic)
+
+        while True:
+            message = pubsub.get_message()
+            if message:
+                self.chain_process(message.get('data'))
+
+    def process(self, message=None):
+        return message
