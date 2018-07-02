4d44381a1ddca24d7782b3dfbe8b4952e3466057
Nhat Vo Van <sunary@Nhats-MacBook-Pro.local>
2018-07-02 21:04:25 +0700

    refactor module

--- ank/apps/schedule_app.py
+++ ank/apps/schedule_app.py
@@ -2,7 +2,7 @@ __author__ = 'sunary'
 
 
 import time
-from ank.base_apps.pipe_app import PipeApp
+from ank.apps.pipe_app import PipeApp
 from datetime import datetime
 from ank.utils.crontab_time import CrontabTimeReader
 
